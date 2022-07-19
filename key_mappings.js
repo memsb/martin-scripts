32c6fb473aec1f9c1ee19a91fd9edf890a874bb0
Simon Tanner <simon.tanner01@bbc.co.uk>
2022-07-19 12:33:55 +0100

    update

--- key_mappings.js
+++ key_mappings.js
@@ -297,7 +297,37 @@ const mapping = {
     "https://repository.api.bbci.co.uk/siege/revisions/head": null,
     "https://repository.api.bbci.co.uk/wrk2/revisions/head": null,
     "https://rpm.nodesource.com/pub_12.x/el/7/x86_64": null,
-    "https://packages.elastic.co/beats/yum/el/x86_64": null,
+    "https://packages.elastic.co/beats/yum/el/x86_64": "-----BEGIN PGP PUBLIC KEY BLOCK-----\n" +
+      "Version: GnuPG v1.4.7 (GNU/Linux)\n" +
+      "\n" +
+      "mQENBFI3HsoBCADXDtbNJnxbPqB1vDNtCsqhe49vFYsZN9IOZsZXgp7aHjh6CJBD\n" +
+      "A+bGFOwyhbd7at35jQjWAw1O3cfYsKAmFy+Ar3LHCMkV3oZspJACTIgCrwnkic/9\n" +
+      "CUliQe324qvObU2QRtP4Fl0zWcfb/S8UYzWXWIFuJqMvE9MaRY1bwUBvzoqavLGZ\n" +
+      "j3SF1SPO+TB5QrHkrQHBsmX+Jda6d4Ylt8/t6CvMwgQNlrlzIO9WT+YN6zS+sqHd\n" +
+      "1YK/aY5qhoLNhp9G/HxhcSVCkLq8SStj1ZZ1S9juBPoXV1ZWNbxFNGwOh/NYGldD\n" +
+      "2kmBf3YgCqeLzHahsAEpvAm8TBa7Q9W21C8vABEBAAG0RUVsYXN0aWNzZWFyY2gg\n" +
+      "KEVsYXN0aWNzZWFyY2ggU2lnbmluZyBLZXkpIDxkZXZfb3BzQGVsYXN0aWNzZWFy\n" +
+      "Y2gub3JnPokBOAQTAQIAIgUCUjceygIbAwYLCQgHAwIGFQgCCQoLBBYCAwECHgEC\n" +
+      "F4AACgkQ0n1mbNiOQrRzjAgAlTUQ1mgo3nK6BGXbj4XAJvuZDG0HILiUt+pPnz75\n" +
+      "nsf0NWhqR4yGFlmpuctgCmTD+HzYtV9fp9qW/bwVuJCNtKXk3sdzYABY+Yl0Cez/\n" +
+      "7C2GuGCOlbn0luCNT9BxJnh4mC9h/cKI3y5jvZ7wavwe41teqG14V+EoFSn3NPKm\n" +
+      "TxcDTFrV7SmVPxCBcQze00cJhprKxkuZMPPVqpBS+JfDQtzUQD/LSFfhHj9eD+Xe\n" +
+      "8d7sw+XvxB2aN4gnTlRzjL1nTRp0h2/IOGkqYfIG9rWmSLNlxhB2t+c0RsjdGM4/\n" +
+      "eRlPWylFbVMc5pmDpItrkWSnzBfkmXL3vO2X3WvwmSFiQbkBDQRSNx7KAQgA5JUl\n" +
+      "zcMW5/cuyZR8alSacKqhSbvoSqqbzHKcUQZmlzNMKGTABFG1yRx9r+wa/fvqP6OT\n" +
+      "RzRDvVS/cycws8YX7Ddum7x8uI95b9ye1/Xy5noPEm8cD+hplnpU+PBQZJ5XJ2I+\n" +
+      "1l9Nixx47wPGXeClLqcdn0ayd+v+Rwf3/XUJrvccG2YZUiQ4jWZkoxsA07xx7Bj+\n" +
+      "Lt8/FKG7sHRFvePFU0ZS6JFx9GJqjSBbHRRkam+4emW3uWgVfZxuwcUCn1ayNgRt\n" +
+      "KiFv9jQrg2TIWEvzYx9tywTCxc+FFMWAlbCzi+m4WD+QUWWfDQ009U/WM0ks0Kww\n" +
+      "EwSk/UDuToxGnKU2dQARAQABiQEfBBgBAgAJBQJSNx7KAhsMAAoJENJ9ZmzYjkK0\n" +
+      "c3MIAIE9hAR20mqJWLcsxLtrRs6uNF1VrpB+4n/55QU7oxA1iVBO6IFu4qgsF12J\n" +
+      "TavnJ5MLaETlggXY+zDef9syTPXoQctpzcaNVDmedwo1SiL03uMoblOvWpMR/Y0j\n" +
+      "6rm7IgrMWUDXDPvoPGjMl2q1iTeyHkMZEyUJ8SKsaHh4jV9wp9KmC8C+9CwMukL7\n" +
+      "vM5w8cgvJoAwsp3Fn59AxWthN3XJYcnMfStkIuWgR7U2r+a210W6vnUxU4oN0PmM\n" +
+      "cursYPyeV0NX/KQeUeNMwGTFB6QHS/anRaGQewijkrYYoTNtfllxIu9XYmiBERQ/\n" +
+      "qPDlGRlOgVTd9xUfHFkzB52c70E=\n" +
+      "=92oX\n" +
+      "-----END PGP PUBLIC KEY BLOCK-----\n",
     "https://repository.api.bbci.co.uk/ibl-audit-el7/revisions/head": null
   }
 
